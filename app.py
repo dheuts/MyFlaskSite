d708f4786dbdd01dfe2c98308de216a987e12956
Dave Heuts <dheuts@deofficespecialist.nl>
2020-05-24 16:41:45 +0200

    itteration 4

--- app.py
+++ app.py
@@ -2,9 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+all_posts = [
+    {
+        'title': 'Post 1',
+        'content': 'This is the content of Post 1',
+        'author': 'David'
+    },
+    {
+        'title': 'Post 2',
+        'content': 'This is the content of Post 2'
+    }
+]
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/posts')
+def posts():
+    return render_template('posts.html', posts=all_posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
