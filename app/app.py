d82f7e1698723f08b81e67c8de665f89dfef9434
Dave Heuts <dheuts@deofficespecialist.nl>
2020-05-27 15:27:21 +0200

    27 mei 15:27

--- app/app.py
+++ app/app.py
@@ -16,18 +16,6 @@ class BlogPost(db.Model):
     def __repr__(self):
         return 'Blog post ' + str(self.id)
 
-all_posts = [
-    {
-        'title': 'Post 1',
-        'content': 'This is the content of Post 1',
-        'author': 'David'
-    },
-    {
-        'title': 'Post 2',
-        'content': 'This is the content of Post 2'
-    }
-]
-
 @app.route('/')
 def index():
     return render_template('index.html')
